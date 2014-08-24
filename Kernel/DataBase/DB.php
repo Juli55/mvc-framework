6032e67e482118fc7f7551b3c1a1d2b5fe29d74f
Dennis Eisele <dennis.eisele@online.de>
2014-08-24 07:59:48 +0200

    update database

--- Kernel/DataBase/DB.php
+++ Kernel/DataBase/DB.php
@@ -2,17 +2,19 @@
 
 /**
  * @author Julian Bertsch <julian.bertsch42@gmail.de>
+ * @author Dennis Eisele  <dennis.eisele@online.de>
  */
 Class DB
 {
 	public static $db;
+	public static $db_user;
 
 	function __construct()
 	{
 		self::init('localhost', 'root', '');
 	}
 
-	private function init($host, $username, $password)
+	private static function init($host, $username, $password)
 	{
 		self::$db = new mysqli($host, $username, $password);
 
@@ -21,6 +23,8 @@ Class DB
 		  exit();
 		}
 
-		self::$db->close();
+		self::$db_user = "projectify";
+		return self::$db;
 	}
 }
+?>  
