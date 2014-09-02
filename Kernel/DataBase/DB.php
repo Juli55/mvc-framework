753bdbf17711dda35d917135181ed0554721de2b
Julian Bertsch <julian.bertsch42@gmail.com>
2014-09-02 20:58:53 +0200

    db change

--- Kernel/DataBase/DB.php
+++ Kernel/DataBase/DB.php
@@ -2,25 +2,46 @@
 
 /**
  * @author Julian Bertsch <julian.bertsch42@gmail.de>
+ * @author Dennis Eisele  <dennis.eisele@online.de>
  */
+
 Class DB
 {
+
 	public static $db;
 
+	public static $db_user;
+
+
 	function __construct()
+
 	{
+
 		self::init('localhost', 'root', '');
+
 	}
 
-	private function init($host, $username, $password)
+
+	private static function init($host, $username, $password)
+
 	{
+
 		self::$db = new mysqli($host, $username, $password);
 
+
 		if (mysqli_connect_errno()) {
+
 		  printf("Verbindung fehlgeschlagen: %s\n", mysqli_connect_error());
+
 		  exit();
+
 		}
 
-		self::$db->close();
+
+		self::$db_user = "projectify";
+
+		return self::$db;
+
 	}
+
 }
