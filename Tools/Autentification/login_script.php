741c6ec0fc43fc6ab2208d06b90a92b6f3d1231e
Dennis Eisele <dennis.eisele@online.de>
2014-10-12 04:32:40 +0200

    loginscript added

--- Tools/Autentification/login_script.php
+++ Tools/Autentification/login_script.php
@@ -30,7 +30,7 @@ class login_script{
 		{
 			$em->find('email',$email);
 
-			if(!empty($entity->getEmail() && !empty($entity->getPassword())
+			if(!empty($entity->getEmail() && ($entity->getPassword() === $password)
 			{
 				$request->setSession('userid',$entity->getID());
 			}
