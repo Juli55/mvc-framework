01be607c69ef8fceb5cf63c481c35526588d59be
Julian Bertsch <Julian.bertsch@web.de>
2014-10-26 13:50:46 +0100

    add sync entity-db

--- Kernel/ConsoleFunctions/Database.php
+++ Kernel/ConsoleFunctions/Database.php
@@ -3,14 +3,15 @@ namespace Kernel\ConsoleFunctions;
 
 use Kernel\DataBase\DB;
 use Config\DBConfig;
+use Config\SrcInit;
 
 class Database
 {
 	public static function create()
 	{
 		// init DB
-		$db = new DB();
-		DBConfig::init();
+			$db = new DB();
+			DBConfig::init();
 
 		foreach(DBConfig::getDatabases() as $key => $value){
 
@@ -33,4 +34,109 @@ class Database
 			}
 		}
 	}//end function
+
+	public static function sync()
+	{
+		// init srcFolders
+			SrcInit::init();
+		// init DB
+			$db = new DB();
+			DBConfig::init();
+
+		// read entitys from initialized srcFolders
+			foreach(SrcInit::getSrcFolder() as $srcFolder){
+				if(file_exists('src/'.$srcFolder.'/Entity')){
+					$handle = opendir ('src/'.$srcFolder.'/Entity'); 
+					while (false !== $entityFile = readdir($handle)) {
+
+						if ($entityFile != "." && $entityFile != "..") {
+				        	
+				        	include 'src/'.$srcFolder.'/Entity/'.$entityFile;
+				        	$entityName =  rtrim($entityFile,'.php');
+				        	$entityObject = new $entityName;
+
+				        	// check if every Variable has a setter and getter method
+				        		$entityObjectClean = array();
+				        		foreach ((array)$entityObject as $key => $value){
+							        $key = preg_match('/^\x00(?:.*?)\x00(.+)/', $key, $matches) ? $matches[1] : $key;
+							        $entityObjectClean[$key] = $value;
+							    }
+							    foreach ($entityObjectClean as $key => $value) {
+							    	if(!method_exists($entityObject,'set'.ucfirst($key)) || !method_exists($entityObject,'get'.ucfirst($key))){
+							    		die("\nYou have an mistake in your Entity: $entityName, because you don't have all setter and getter methods!\n");
+							    	}
+							    }
+							// check if database exists
+							    $dbName = DBConfig::getDatabases()['primary'];
+							    $sql_check = "SHOW DATABASES LIKE '$dbName'";
+								$result = $db::$db->query($sql_check);
+								if(mysqli_num_rows($result) > 0){
+								    
+								    	// check if database table exist
+								    		$sql_check ="SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '$entityName' AND table_schema = '$dbName' ";
+								    		$result = $db::$db->query($sql_check);
+								    		
+								    		if($result->num_rows > 0)
+											{
+												foreach ($entityObjectClean as $key => $value) {
+									    			echo $key;
+									    		}
+											}else{
+												$sql = "CREATE TABLE IF NOT EXISTS $dbName.`$entityName` (
+	 
+														`id` int(11) NOT NULL AUTO_INCREMENT,
+	 
+														`name` varchar(100) NOT NULL,
+	 
+														`email` varchar(150) NOT NULL,
+	 
+														`address` varchar(255) NOT NULL,
+	 
+														`mob` varchar(15) NOT NULL,
+	 
+														PRIMARY KEY (`id`)
+	 
+														)";
+									    		$db::$db->query($sql) or die($db::$db->error);
+											}
+											/*
+									    	if(mysqli_num_rows($result) > 0){
+									    		//echo "hey";
+									    		foreach ($entityObjectClean as $key => $value) {
+									    			
+									    		}
+								    		}else{
+								    			$sql = "CREATE TABLE IF NOT EXISTS $dbName.`userese` (
+	 
+														`id` int(11) NOT NULL AUTO_INCREMENT,
+	 
+														`name` varchar(100) NOT NULL,
+	 
+														`email` varchar(150) NOT NULL,
+	 
+														`address` varchar(255) NOT NULL,
+	 
+														`mob` varchar(15) NOT NULL,
+	 
+														PRIMARY KEY (`id`)
+	 
+														)";
+									    		$db::$db->query($sql) or die($db::$db->error);
+								    			foreach ($entityObjectClean as $key => $value) {
+
+									    		}
+								    		}
+								    		*/
+								    
+								}else{
+									die("\n You have to create the Databases first\n");
+								}
+
+
+				        }
+					}
+					closedir($handle); 
+				}
+			}
+	}
 }
