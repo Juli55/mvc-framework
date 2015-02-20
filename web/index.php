a71687dbd0c0bbf06cfdde32629e758cc1fdfe4f
Julian Bertsch <Julian.bertsch@web.de>
2015-02-20 21:19:22 +0100

    cache: index cleaned

--- web/index.php
+++ web/index.php
@@ -1,12 +1,10 @@
 <?php
 
-use Kernel\Controller;
 use Kernel\RoutingEngine\RoutingEngine;
-use profile\Controller\profileController;
 
-function __autoload($class_name)
+function __autoload($className)
 {
-    include '..\\'.$class_name . '.php';
+    include '..\\'.$className . '.php';
 }
 
 final class App
@@ -16,43 +14,51 @@ final class App
 	 */
 	private static $uri;
 
-	function __construct()
+	/**
+	 *
+	 * The AppConstructor is the reads the requestUri store it and calls the routingMethod
+	 *
+	 * @return void
+	 */
+	public function __construct()
 	{
-		if(isset($_SERVER['PATH_INFO'])){
+		//set the uri
+			if(isset($_SERVER['PATH_INFO'])){
 
-			self::$uri = $_SERVER['PATH_INFO'];
+				self::$uri = $_SERVER['PATH_INFO'];
+
+			}elseif($_SERVER['SCRIPT_NAME'] === $_SERVER['PHP_SELF']){
 				
-		}elseif($_SERVER['SCRIPT_NAME'] === $_SERVER['PHP_SELF']){
-			
-			if($_SERVER['REQUEST_URI'] !== '/'){
-				
-				self::$uri = ltrim($_SERVER['REQUEST_URI'],dirname($_SERVER['PHP_SELF']));
+				if($_SERVER['REQUEST_URI'] !== '/'){
+					
+					self::$uri = ltrim($_SERVER['REQUEST_URI'],dirname($_SERVER['PHP_SELF']));
+				}else{
+
+					self::$uri = $_SERVER['REQUEST_URI'];
+				}
 			}else{
-
 				self::$uri = $_SERVER['REQUEST_URI'];
 			}
-
-		}else{
-
-			self::$uri = $_SERVER['REQUEST_URI'];
-		}
-
-		self::routing();
+		//call the routingMethod
+			self::routing();
 	}
 
+	/**
+	 *
+	 * The routingMethod starts the session calls the RoutingEngine and echo the returnValue
+	 *
+	 * @return void
+	 */
 	private static function routing()
 	{
-			
-		if(self::$uri !== $_SERVER['PHP_SELF'] && self::$uri !== '/'){
-			
-			session_start();
-			$routingEngine = new RoutingEngine;
-			echo $routingEngine->handleRouting(self::$uri);
-
-		}else{
-			
-			echo 'root';
-		}
+		//if the URI calls not the index.php then start session and echo the Routing, else it echo that the user is in the root
+			if(self::$uri !== $_SERVER['PHP_SELF'] && self::$uri !== '/'){	
+				session_start();
+				$routingEngine = new RoutingEngine;
+				echo $routingEngine->handleRouting(self::$uri);
+			}else{
+				echo 'root';
+			}
 	}
 		
 }
