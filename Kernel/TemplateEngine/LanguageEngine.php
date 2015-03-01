b4c4d7a6b6eb033e7f6e2f69e5f0ad8232842767
Dennis Eisele <dennis.eisele@online.de>
2015-03-01 04:59:19 +0100

    cache: start with LanguageEngine

---
+++ Kernel/TemplateEngine/LanguageEngine.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kernel\LanguageEngine;
+
+use // einfügen
+
+/**
+ * @author Dennis Eisele <dennis.eisele@online.de>
+ */
+ class LanguageEngine
+ {
+ 	/**
+ 	 * @var
+ 	 */
+
+
+ 	/**
+ 	 *
+ 	 * a function
+ 	 *
+ 	 * @param
+ 	 *
+ 	 * @return
+ 	 */
+ 	public function()
+ 	{
+
+ 	}
+ }
