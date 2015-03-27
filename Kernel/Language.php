28ef5f1a67af0880e9f68bb725150dabb7fdce9b
Julian Bertsch <Julian.bertsch@web.de>
2015-03-27 23:14:58 +0100

    ready: created an object with to set and get the Language

---
+++ Kernel/Language.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Kernel;
+
+use Kernel\HttpKernel\Request;
+
+/**
+ * @author Julian Bertsch <julian.bertsch42@gmail.de>
+ */
+class Language
+{
+	/**
+	 * @var string
+	 */
+	private $language;
+
+	/**
+	 *
+	 * The Constructor calls the setLanguage
+	 *
+	 * @return void
+	 */
+	public function __construct($language = '')
+	{
+		$this->language = $this->setLanguage($language);
+	}
+
+	/**
+	 *
+	 * The setLanguage Function returns the browserLanguage or custom
+	 *
+	 * @param string $language
+	 *
+	 * @return void
+	 */
+	private function setLanguage($language)
+	{
+		if(empty($language)){
+			$request = new Request();
+			return $request->server['HTTP_ACCEPT_LANGUAGE'];
+		}else{
+			return $language;	
+		}
+	}
+
+	/**
+	 *
+	 * this Function returns the Language
+	 *
+	 * @param string $language
+	 *
+	 * @return string
+	 */
+	private function getLanguage()
+	{
+		return $this->language;
+	}
+}
