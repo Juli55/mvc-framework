e68d268c4834b88c010a8c703988bcd3c3d39a5e
Julian Bertsch <Julian.bertsch42@gmail.com>
2015-05-29 04:30:37 +0200

    cache: set the method-comments

--- app/Tools/DateTime/DateTime.php
+++ app/Tools/DateTime/DateTime.php
@@ -21,39 +21,87 @@ class DateTime
 		}
 	}
 
+	/**
+	 *
+	 * The getCurrentTimeDefault function returns the current datetime with the default timezone
+	 * 
+	 * @param 
+	 *
+	 * @return string 
+	 */
 	public function getCurrentTimeDefault()
 	{
 		$dateTime = new \DateTime('now', new \DateTimeZone($this->defaultTimezone));
 		return $dateTime->format('d.m.Y H:i:s');
 	}
 
+	/**
+	 *
+	 * The getCurrentTimeSpecific function returns the current datetime with the timezone from parameter
+	 * 
+	 * @param 
+	 *
+	 * @return string 
+	 */
 	public function getCurrentTimeSpecific($timezone)
 	{
 		$dateTime = new \DateTime('now', new \DateTimeZone($timezone));
 		return $dateTime->format('d.m.Y H:i:s');
 	}
 
+	/**
+	 *
+	 * The getTimeDefault function returns the time from parameter and returns the time with default timezone
+	 * 
+	 * @param string $datetime
+	 *
+	 * @return string 
+	 */
 	public function getTimeDefault($datetime)
 	{
 		$dateTime = new \DateTime($datetime, new \DateTimeZone($this->defaultTimezone));
 		return $dateTime->format('d.m.Y H:i:s');
 	}
 
+	/**
+	 *
+	 * The getTimeSpecific function returns the time from parameter and returns the time with specific timezone
+	 * 
+	 * @param string $datetime, $timezone
+	 *
+	 * @return string 
+	 */
 	public function getTimeSpecific($datetime, $timezone)
 	{
 		$dateTime = new \DateTime($datetime, new \DateTimeZone($timezone));
 		return $dateTime->format('d.m.Y H:i:s');
 	}
 
-	public function changeToDefault($timezone, $time)
+	/**
+	 *
+	 * The changeToDefault function changes an specific datetime to the default
+	 * 
+	 * @param string $datetime, $timezone
+	 *
+	 * @return string 
+	 */
+	public function changeToDefault($datetime, $timezone)
 	{
-		$dateTime = new \DateTime($time, new \DateTimeZone($timezone));
+		$dateTime = new \DateTime($datetime, new \DateTimeZone($timezone));
 		$dtz 	  = new \DateTimeZone($this->defaultTimezone);
 		$dateTime->setTimezone($dtz);
 		return $dateTime->format();
 	}
 
-	public function changeToSpecific($timezone, $time, $format)
+	/**
+	 *
+	 * The changeToSpecific function changes an default datetime to an specific
+	 * 
+	 * @param string $datetime, $timezone
+	 *
+	 * @return string 
+	 */
+	public function changeToSpecific($datetime, $timezone, $format)
 	{
 		$dateTime = new \DateTime($time, new \DateTimeZone($this->defaultTimezone));
 		$dtz = new \DateTimeZone($timezone);
